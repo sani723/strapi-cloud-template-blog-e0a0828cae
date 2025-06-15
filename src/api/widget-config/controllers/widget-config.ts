9c7ef1e71ef976b8fb9984618d11304da12a1e15
Sajjad <sani723@hotmail.com>
2025-06-15 22:16:17 +0400

    feat: add local widget + component work

---
+++ src/api/widget-config/controllers/widget-config.ts
@@ -0,0 +1,7 @@
+/**
+ * widget-config controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::widget-config.widget-config');
---
+++ src/api/widget-config/routes/widget-config.ts
@@ -0,0 +1,7 @@
+/**
+ * widget-config router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::widget-config.widget-config');
